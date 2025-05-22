c873c12bf1fa4c792187aa76cbcd76ded5a84c66
Spatio-temporal-prediction <119410940+Spatio-temporal-prediction@users.noreply.github.com>
2025-05-22 11:00:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,41 +33,30 @@ class MambaLSTMPredictor(nn.Module):
         x=x.permute(0,1,3,4,2)
         x=self.dynamic_proj(x)
         x=x.permute(0,1,4,2,3)
-
         if self.states != None:
-            self.states = None
-            
+            self.states = None   
         long_out=[]
         outputs = []
         inputs_len = x.shape[1]
-
-
         assert time_features.shape[1] == inputs_len + targets_len, f"{time_features.shape[1]}"
         last_input = x[:, -1] 
         time_embeding=self.time_embed(time_features)
-
-
         for i in range(6):
             x_step=x[:, i]
             t_step = time_embeding[:, i]
             x_step = self.se_time_fusion(x_step, t_step)
             output, self.states = self.MambaLSTM(x_step, self.states)
             long_out.append(output)
-
         for i in range(6,inputs_len - 1):
             x_step=x[:, i]
             t_step = time_embeding[:, i]  
             x_step = self.se_time_fusion(x_step, t_step)
             output, self.states = self.MambaLSTM(x_step, self.states)
             outputs.append(output)
-
-
         t_step = time_embeding[:, inputs_len-1]  
         last_input =self.se_time_fusion(last_input, t_step)
         output, self.states = self.MambaLSTM(last_input, self.states)
-
         dec_out=output
-
         out = self.fc(dec_out.permute(0, 2, 3, 1).contiguous()).permute(0, 3, 1, 2).contiguous()
         return out
 
@@ -80,11 +69,7 @@ class Main_MambaLSTM(nn.Module):
                                          depths=2, pre_len=pre_len, drop_rate=0.0, drop_path_rate=0.1)
 
     def forward(self, input,time_feature):
-
         out=self.Main_MambaLSTM(input,time_feature)
-
         return out
 
-    def parameters(self):
-        return list(self.MambaLSTM.parameters())
 
